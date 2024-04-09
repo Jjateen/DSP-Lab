67312c3d8f8d71a966b12922c5fc08179c907e85
jjateen <jjateen97@gmail.com>
2024-04-09 11:32:00 +0530

    Add circshift and timerev

--- Experiment08/timerev.m
+++ Experiment08/timerev.m
@@ -3,17 +3,15 @@ clear all
 close all
 
 x_n = [0 5 8 1];
-k = 2;
+n = length(x_n);
 
-shifted_x_n = shiftcirc(x_n,k);
+reversed_x_n = zeros(1, n);
+for i = 1:n
+    reversed_x_n(i) = x_n(n - i + 1);
+end
 
 disp('Original vector:');
 disp(x_n);
-
-disp('Shifted vector:');
-disp(shifted_x_n);
-
-reversed_x_n = x_n(end:-1:1);
 
 disp('Time-reversed vector:');
 disp(reversed_x_n);

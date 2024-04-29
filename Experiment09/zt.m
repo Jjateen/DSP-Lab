9a4243258ccfcda2be1ef091e9a8f4928cac5766
jjateen <jjateen97@gmail.com>
2024-04-29 14:47:35 +0530

    Update Exp09

--- Experiment09/zt.m
+++ Experiment09/zt.m
@@ -1,4 +1,4 @@
-function [z_x] = zt(n, g_1_n)
+function [Xz] = zt(n, g_1_n)
     re_z=-2:0.01:2;
     im_z=-2:0.01:2;
     for ii=1:length(re_z)
@@ -20,19 +20,19 @@ function [z_x] = zt(n, g_1_n)
     
     for ii=1:length(re_z)
         for jj=1:length(im_z)
-            z_x(ii,jj)=sum(abs(G_1_z(ii,jj,:)));
+            Xz(ii,jj)=sum(abs(G_1_z(ii,jj,:)));
         end
     end
     subplot(3,1,1), stem(n, g_1_n)
     ylabel("x(n)");
     xlabel("n");
-    subplot(3,1,2),surf(X, Y, 10*log10(z_x), 'linestyle','none');
+    subplot(3,1,2),surf(X, Y, 10*log10(Xz), 'linestyle','none');
     colorbar('Direction','reverse')
     colormap(jet);
     ylabel("Image(z)");
     xlabel("Real(z)");
     
-    subplot(3,1,3),image( [re_z(1,1), re_z(1,end)], [im_z(1, end), im_z(1,1)] ,10*log10(z_x));
+    subplot(3,1,3),image( [re_z(1,1), re_z(1,end)], [im_z(1, end), im_z(1,1)] ,10*log10(Xz));
     colorbar('Direction','reverse')
     ylabel("Image(z)");
     xlabel("Real(z)");
